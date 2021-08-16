e436247924e4f7aaa298fe6e512c5bb605f5e1ce
Satoshi Matsushima <>
2021-08-17 00:15:17 +0900

    create Title component and form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import './App.css';
+import Form from './components/Form';
+import Title from './components/Title';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Title />
+      <Form />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Form.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Form() {
+  return (
+    <form>
+      <input type="text" name="city" placeholder="City" />
+      <button type="submit">Get Weather</button>
+    </form>
+  );
+}
+
+export default Form;
