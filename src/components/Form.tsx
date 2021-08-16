7c78215e84e960b80329acf84da9d4716a0dce27
Satoshi Matsushima <>
2021-08-17 08:35:41 +0900

    update Form.tsx fetching weather API

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Form() {
+  const [city, setCity] = useState<string>('');
+
+  const getWeather = (e: any) => {
+    e.preventDefault();
+    fetch(
+      'https://api.weatherapi.com/v1/current.json?key=d29317caee084dbfa93152740211608&q=London&aqi=no',
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
   return (
     <form>
-      <input type="text" name="city" placeholder="City" />
-      <button type="submit">Get Weather</button>
+      <input
+        type="text"
+        name="city"
+        placeholder="City"
+        onChange={(e) => setCity(e.target.value)}
+      />
+
+      <button type="submit" onClick={getWeather}>
+        Get Weather
+      </button>
     </form>
   );
 }
